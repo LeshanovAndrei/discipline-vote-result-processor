82efb4f03b64e4894bbeb11ca6d0eb3b9d174493
LeshanovAndrei <leshanov.andrei@gmail.com>
2021-03-19 15:12:45 +0200

    Selection added

---
+++ DVRP1/DVRP1/Selection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVRP1
+{
+    class Selection
+    {
+        public void AddStudent(Student st)
+        {
+            students.Add(st);
+            numbers++;
+            //Какая-нибудь проверка статуса
+        }
+        private Discipline discipline;
+        private List<Student> students;
+        private uint numbers;
+        private uint status;
+        public Discipline Discipline
+        {
+            get
+            {
+                return discipline;
+            }
+            set
+            {
+                discipline = value;
+            }
+        }
+        
+        public uint Numbers
+        {
+            get
+            {
+                return numbers;
+            }
+            set
+            {
+                numbers = value;
+            }
+        }
+        
+        public uint Status
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                status = value;
+            }
+        }
+    }
+}
